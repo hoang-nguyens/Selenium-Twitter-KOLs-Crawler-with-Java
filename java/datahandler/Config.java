6da270d37f3514adbb4c627ae527d0ed560c05f7
unknown <hoang0403ss@gmail.com>
2024-12-24 14:48:28 +0700

    add java again

---
+++ java/datahandler/Config.java
@@ -0,0 +1,21 @@
+package datahandler;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Config {
+    // Centralized usernames and passwords
+    public static final List<String> USERNAMES = Arrays.asList(
+            "add username"
+    );
+
+    public static final List<String> PASSWORDS = Arrays.asList(
+            "add password"
+    );
+
+    // Centralized keywords
+    public static final List<String> KEYWORDS = Arrays.asList(
+            "blockchain", "bitcoin", "crypto", "DeFi", 
+            "Ethereum", "NFT", "SmartContract", "Web3"
+    );
+}
